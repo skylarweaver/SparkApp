58b30a5f46d0f497ea5fc772a0e06efa1daffbc9
Nathan Oh <njo@andrew.cmu.edu>
2015-11-18 01:32:08 -0500

    worked on borrow detail page

--- mobile/www/js/app.js
+++ mobile/www/js/app.js
@@ -52,16 +52,15 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
     }
   })
 
-  // .state('tab.borrow-detail', {
-  //   url: '/borrow/:deviceID',
-  //   views: {
-  //     'tab-borrow': {
-  //       templateUrl: 'templates/borrow-detail.html',
-  //       controller: 'BorrowDetailCtrl'
-  //     }
-  //   }
-  // })
-
+  .state('tab.borrow-detail', {
+    url: '/borrow/:owned_deviceID',
+    views: {
+      'tab-borrow': {
+        templateUrl: 'templates/borrow-detail.html',
+        controller: 'BorrowDetailCtrl'
+      }
+    }
+  })
 
   .state('tab.lend', {
     url: '/lend',
@@ -72,6 +71,16 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
       }
     }
   })
+
+  .state('tab.chat', {
+    url: '/chats',
+    views: {
+      'tab-chats': {
+        templateUrl: 'templates/tab-chats.html',
+         controller: 'ChatsCtrl'
+      }
+    }
+  })  
 
   .state('tab.chat-detail', {
     url: '/chats/:chatId',
