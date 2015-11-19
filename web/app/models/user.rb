ef80deeb1054413c0df2f22aa4a1d1a5aada8954
Skylar Weaver <sweaver@andrew.cmu.edu>
2015-11-19 15:03:54 -0500

    Each user now has token and token is tracked throughout app. ALSO app now remains logged in after refresh

--- web/app/models/user.rb
+++ web/app/models/user.rb
@@ -4,6 +4,10 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  # JSON auth_token
+  acts_as_token_authenticatable
+  
+
   # relationships
   has_many :owned_devices
   has_many :devices, through: :owned_devices
--- web/db/schema.rb
+++ web/db/schema.rb
@@ -59,8 +59,10 @@ ActiveRecord::Schema.define(version: 20151110192228) do
     t.datetime "updated_at",                          null: false
     t.string   "first_name"
     t.string   "last_name"
+    t.string   "authentication_token"
   end
 
+  add_index "users", ["authentication_token"], name: "index_users_on_authentication_token"
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
