fea8cb49d1e6e9c7b2ef936323f73568ff5cff1f
Aditi Sarkar <aditi0821@gmail.com>
2015-11-22 12:56:24 -0500

    switched to using owned devices as endpoint

--- web/app/models/owned_device.rb
+++ web/app/models/owned_device.rb
@@ -1,4 +1,13 @@
 class OwnedDevice < ActiveRecord::Base
 	belongs_to :device
   	belongs_to :user
+
+  	def device_name 
+  		Device.find(self.device_id).name
+  	end
+
+  	def charger_name
+  		Charger.find(Device.find(self.device_id).charger_id).name
+  	end
+
 end
