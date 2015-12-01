7f55e715a96dbf53ff970dc0d7b43ef3b3bf58ae
Aditi Sarkar <aditi0821@gmail.com>
2015-12-01 18:08:27 -0500

    add device works now!

--- web/app/models/transaction.rb
+++ web/app/models/transaction.rb
@@ -1,3 +1,4 @@
 class Transaction < ActiveRecord::Base
-	belongs_to :user
+	belongs_to :borrowers, foreign_key: "borrower_id", class_name: "User"
+	belongs_to :lender, foreign_key: "lender_id", class_name: "User"
 end
