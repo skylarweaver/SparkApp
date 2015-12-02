cac98b61d52570d63f6437a0adb40b4bf6410df7
Skylar Weaver <sweaver@andrew.cmu.edu>
2015-12-01 22:16:05 -0500

    delete now works

--- web/app/serializers/user_serializer.rb
+++ web/app/serializers/user_serializer.rb
@@ -1,9 +1,9 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :user_email, :first_name, :last_name, :token, :user_token, :last_sign_in_at, :created_at
+  attributes :id, :user_email, :first_name, :last_name, :token, :user_token, :created_at
   
-  def last_sign_in_at
-    object.last_sign_in_at.strftime("%d/%m/%y %I:%M %p")
-  end
+  # def last_sign_in_at
+  #   object.last_sign_in_at.strftime("%d/%m/%y %I:%M %p")
+  # end
   
   def token
     user_token
