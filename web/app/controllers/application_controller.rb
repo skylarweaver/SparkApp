1bbbe7ab597befb78abf48cb9a154f2c8e4e4168
Aditi Sarkar <aditi0821@gmail.com>
2015-12-02 13:02:42 -0500

    contact lender page updated

--- web/app/controllers/application_controller.rb
+++ web/app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
 	respond_to :html, :json
+	serialization_scope :view_context
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   #protect_from_forgery with: :
