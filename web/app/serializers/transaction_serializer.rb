bce237d63ae736984ae630ce0ffb3352f465a56f
Aditi Sarkar <aditi0821@gmail.com>
2015-12-15 22:23:29 -0500

    fixed some bugs also can create transaction that persists

--- web/app/serializers/transaction_serializer.rb
+++ web/app/serializers/transaction_serializer.rb
@@ -1,12 +1,12 @@
 class TransactionSerializer < ActiveModel::Serializer
-  attributes :id, :charger_id, :lender_id, :lender_user, :borrower_id, :length_time_requested, :start_time, :end_time, :accepted
+  attributes :id, :charger_id, :lender_id, :lender_user, :borrower_user, :borrower_id, :length_time_requested, :start_time, :end_time, :accepted
 
   def lender_user
     User.find(object.lender_id)
   end
 
   def borrower_user
-    User.find(object.lender_id)
+    User.find(object.borrower_id)
   end
 
 end
